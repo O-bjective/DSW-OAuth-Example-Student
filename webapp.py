29829e9095bde202c66b44e4bb6c990cf969cd13
Jaryd Chandos <jarydchandos.5@gmail.com>
2019-02-11 10:30:39 -0800

    J

--- webapp.py
+++ webapp.py
@@ -58,7 +58,7 @@ def authorized():
         try:
             #save user data and set log in message
             session['github_token'] = (esp['access_token'], '')
-            session['user.data'] = guthub.get('user').data
+            session['user.data'] = github.get('user').data
             message='You were successfully logged in as ' + session['user.data']['login']
         except Exception as inst:
             session.clear()
